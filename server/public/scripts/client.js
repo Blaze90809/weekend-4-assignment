a998ad0b29018704f6fbe7de3c4a3be253cd2230
Blaze Fugina <blaze.fugina@gmail.com>
2018-02-27 17:33:21 -0600

    updates.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,27 +1,30 @@
 var lifeStory = angular.module('lifeStory', []);
 
-lifeStory.controller('lifeController', function($http) {
+lifeStory.controller('lifeController', function ($http) {
+    console.log('controller loaded')
     var life = this;
 
-//Group of photos for the site
-life.ski = {name: 'ski', location: '../images/skiing.jpg', description: 'A great day to ski.'}
-life.couple = {name: 'couple', location: '../images/couple.jpg', description: 'A lovely couple.'}
-life.mercury = {name: 'mercury', location: '../images/mercury.jpg', description: 'My family\'s dog.'}
-life.twinsgame = {name: 'Twins', location: '../images/twinsgame.jpg', description: 'Play ball!'}
-life.fall = {name: 'Fall', location: '../images/fall.jpg', description: 'A beautiful fall day.'}
-life.wild = {name: 'Wild', location: '../images/wild.jpg', description: 'A fun day to watch the Wild.'}
-//Object to send.
-life.photosIn = [life.couple, life.ski, life.mercury, life.twinsgame, life.fall, life.wild];
+    life.pictures = [];
+
+    life.getPhotos = function () {
+        $http.get('/pictures').then(function (response) {
+            console.log(response.data)
+            life.pictures = response.data;
+        }).catch(function (error) {
+            console.log('error', error)
+        })
+
+    }
+    life.getPhotos();
 
 
-
-life.leaveComment = function(photos){
-    var comment = (photos.commentIn);
-    var photosAdd = angular.copy(photos);
-    var name = (photosAdd.name);
-    life.commentToDom = comment;
-    console.log(life.commentToDom);
-}
+    life.leaveComment = function (photos) {
+        var comment = (photos.commentIn);
+        var photosAdd = angular.copy(photos);
+        var name = (photosAdd.name);
+        life.commentToDom = comment;
+        console.log(life.commentToDom);
+    }
 
 
 });
--- server/server.js
+++ server/server.js
@@ -5,8 +5,20 @@ var port = process.env.PORT || 5000;
 app.use(express.static('server/public'));
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+var pictures = [ski = {name: 'ski', location: '../images/skiing.jpg', description: 'A great day to ski.'},
+couple = {name: 'couple', location: '../images/couple.jpg', description: 'A lovely couple.'},
+mercury = {name: 'mercury', location: '../images/mercury.jpg', description: 'My family\'s dog.'},
+twinsgame = {name: 'Twins', location: '../images/twinsgame.jpg', description: 'Play ball!'},
+fall = {name: 'Fall', location: '../images/fall.jpg', description: 'A beautiful fall day.'},
+wild = {name: 'Wild', location: '../images/wild.jpg', description: 'A fun day to watch the Wild.'}]
+
+app.get('/pictures', function(req, res){
+    console.log('message received')
+    res.send(pictures)
+})
 
 app.listen(port, function(){
     console.log('listening on port', port);
